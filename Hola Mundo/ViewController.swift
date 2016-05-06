6120b93c21479e64f34f2a1699ac55e89d362836
Cesar Acosta <cesaracosta@macbook-pro-de-cesar.local>
2016-05-05 19:56:19 -0700

    Initial Commit

---
+++ Hola Mundo/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Hola Mundo
+//
+//  Created by Cesar Acosta on 02/05/16.
+//  Copyright © 2016 NextU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func changeMessage(sender: AnyObject) {
+        messageLabel.text = "Hola \(nameTextField.text)"
+        view.endEditing(true)
+    }
+
+}
+
